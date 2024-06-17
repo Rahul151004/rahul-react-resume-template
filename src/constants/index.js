2e9469408f10b7806536907ed578aa118d2c2e20
Rahul <rbhat5480@gmail.com>
2024-06-17 19:06:01 +0530

    projects updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,17 +4,35 @@ import css from "../assets/css.png";
 import python from "../assets/python.png"
 import cpp from "../assets/c++.png"
 import java from "../assets/java.png"
+import campusbuzz from "../assets/campusbuzz.png"
 
 export const projectData = [
   {
     image:
+      campusbuzz,
+    title: "Review Website",
+    sentences: [
+      "CampusBuzz is a comprehensive web platform designed to address the challenge of navigating and discovering essential facilities and recreational venues within Lovely Professional University's campus. Our mission is to empower students and campus enthusiasts with the knowledge and accessibility to uncover hidden campus treasures, ranging from quaint eateries to serene study spots and vibrant hangouts.",
+    ],
+    link: "https://campusbuzzlpu.netlify.app",
+  },
+  {
+    image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1a9oACOtjODT2YXFIUmYqJq6ovhEd8VF7Fg&s",
     title: "Ecomm Website",
     sentences: [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
       "amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
+    ],
+    link: "https://www.google.co.in",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1a9oACOtjODT2YXFIUmYqJq6ovhEd8VF7Fg&s",
+    title: "Ecomm Website",
+    sentences: [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
+      "amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis."
     ],
     link: "https://www.google.co.in",
   },
@@ -25,32 +43,6 @@ export const projectData = [
     sentences: [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
       "amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-    ],
-    link: "https://www.google.co.in",
-  },
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1a9oACOtjODT2YXFIUmYqJq6ovhEd8VF7Fg&s",
-    title: "Ecomm Website",
-    sentences: [
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-    ],
-    link: "https://www.google.co.in",
-  },
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1a9oACOtjODT2YXFIUmYqJq6ovhEd8VF7Fg&s",
-    title: "Ecomm Website",
-    sentences: [
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
-      "consectetur adipisicing elit. Commodi vitae fugiat, ad corporis cumque incidunt perferendis maxime minus excepturi sunt ab nobis illum laudantium minima possimus rerum distinctio ut quis.",
     ],
     link: "https://www.google.co.in",
   },
